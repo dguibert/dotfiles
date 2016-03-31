a34ca0d44f37fa49026c02ee385f2ecf27c4879e
David Guibert <david.guibert@gmail.com>
2016-03-31 12:48:46 +0200

    nixup: update package list

--- .config/nixup/profile.nix
+++ .config/nixup/profile.nix
@@ -9,8 +9,38 @@ with lib;
 
     imperativeNix.enable = true;
 
-    user.packages = with pkgs; [ vim gitAndTools.git-annex ];
+    user.packages = with pkgs; [
+      vim
+      gitAndTools.git-annex
+      mr
+      vcsh
+      gitFull
+      (conky.override { x11Support = false; })
+      fossil
+    ];
 
+    user.pathsToLink = [
+      "/bin"
+      "/etc/xdg"
+      "/info"
+      "/man"
+      "/sbin"
+      "/share/emacs"
+      "/share/vim-plugins"
+      "/share/org"
+      "/share/info"
+      "/share/terminfo"
+      "/share/man"
+      "/share/mr"
+      "/share/icons"
+    ];
+
+    systemd.services.fossil-mpisortv = {
+      wantedBy = [ "default.target" ];
+      description = "Fossil Mpi_SortV";
+      path = [ pkgs.fossil ];
+      script = "fossil server ~/code/mpi_sortv/mpi_sortv.fossil --port 8055";
+    };
   };
 
 }
