265788aae5494edb734e99251793d684e9d0841d
David Guibert <david.guibert@gmail.com>
2016-04-22 08:23:40 +0200

    xsession: fix on vbox-57nvj72

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -4,6 +4,7 @@
     ./dwm.patches/0002-apply-dwm-6.1-systray.diff.patch
     ./dwm.patches/0003-config.h-azerty.patch
     ./dwm.patches/0004-config.h-audio-controls.patch
+    ./dwm.patches/0005-solarized-theme.patch
   ];
   allowUnfree = true;
 }
