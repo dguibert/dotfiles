1be7fbf3f54aac06bd7d6b60cc8859e8c5eb54bd
David Guibert <david.guibert@gmail.com>
2017-08-11 16:32:25 +0200

    nix-env: install git-credential-password-store

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -34,6 +34,7 @@ with pkgs.lib;
 	rev = "225582e9a1a9bd9a63e3dfde858f4cc028b07d3e";
 	sha256 = "06ly4qcy0g57jyqnl5q524pcypm85ny4pzac3ljz1dim181zlq3c";
       };
+      preBuild = "ln -s GNUmakefile Makefile";
       installFlags = "PREFIX=$(out)";
       buildInputs = [ gnugrep ];
     };
