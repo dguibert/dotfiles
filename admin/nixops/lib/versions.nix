0f2995d2eb77e79fcff4f8c1d08d5c7069b9302a
David Guibert <david.guibert@gmail.com>
2018-11-23 14:47:54 +0100

    versions: update

--- admin/nixops/lib/versions.nix
+++ admin/nixops/lib/versions.nix
@@ -1,9 +1,9 @@
 let
   # https://vaibhavsagar.com/blog/2018/05/27/quick-easy-nixpkgs-pinning/
-  fetcher = { owner, repo, rev, sha256
+  fetcher = { owner?null, repo?null, rev, sha256
     , url ? "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz"
     }: builtins.fetchTarball {
-      inherit sha256;
+      inherit url sha256;
     };
   nixpkgs = fetcher (builtins.fromJSON (builtins.readFile ../versions.json)).nixpkgs;
   inherit (import <nixpkgs> {}) lib writeScript;
@@ -14,6 +14,7 @@ let
   updater = writeScript "updater.sh" ''
     #!/usr/bin/env bash
     ${./updater} versions.json nixpkgs pu
+    ${./updater} versions.json nixpkgs-overlays pu
     ${./updater} versions.json krops master
     ${./updater} versions.json haskell-updates haskell-updates
     ${./updater} versions.json nixos-17.09 nixos-17.09
--- admin/nixops/pkgs-pinned-overlay.nix
+++ admin/nixops/pkgs-pinned-overlay.nix
@@ -15,14 +15,14 @@ in self: super: {
   ##  # # The Hackage tarball is purposefully broken, because it's not intended to be, like, useful.
   ##  ## https://git-annex.branchable.com/bugs/bash_completion_file_is_missing_in_the_6.20160527_tarball_on_hackage/
   ##  git-annex = super.haskell.lib.dontCheck pkgs_haskell_updates.gitAndTools.git-annex;
-    git-annex = super.haskell.lib.dontCheck (super.haskell.lib.overrideSrc pkgs_haskell_updates.gitAndTools.git-annex {
-      src = super.fetchgit {
-        name = "git-annex-${pkgs_haskell_updates.gitAndTools.git-annex.version}-src";
-        url = "git://git-annex.branchable.com/";
-        rev = "refs/tags/" + pkgs_haskell_updates.gitAndTools.git-annex.version;
-        #upstream via 1bc120fa5faf996a248bdbb4cb7c4327ca368e06
-        sha256 = "0mgmxcr36b86jh56my3vhp9y4cravi0hbppa463q3c21a1cmjc19";
-      };
-    });
+    #git-annex = super.haskell.lib.dontCheck (super.haskell.lib.overrideSrc pkgs_haskell_updates.gitAndTools.git-annex {
+    #  src = super.fetchgit {
+    #    name = "git-annex-${pkgs_haskell_updates.gitAndTools.git-annex.version}-src";
+    #    url = "git://git-annex.branchable.com/";
+    #    rev = "refs/tags/" + pkgs_haskell_updates.gitAndTools.git-annex.version;
+    #    #upstream via 1bc120fa5faf996a248bdbb4cb7c4327ca368e06
+    #    sha256 = "0dnrihpdshrldais74jm5wjfw650i4va8znc1k2zq8gl9p4i8p39";
+    #  };
+    #});
   };
 }
