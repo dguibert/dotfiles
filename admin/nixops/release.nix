f05522f526f8738baf82d42815974c283fb591ac
David Guibert <david.guibert@gmail.com>
2019-01-17 10:08:35 +0100

    release: remove unused recurse function

--- admin/nixops/release.nix
+++ admin/nixops/release.nix
@@ -6,19 +6,6 @@ with (import <nixpkgs/lib>);
 let
   trace = if builtins.getEnv "VERBOSE" == "1" then builtins.trace else (x: y: y);
 
-  # Add the ‘recurseForDerivations’ attribute to ensure that
-  # nix-instantiate recurses into nested attribute sets.
-  recurse = path: attrs:
-    if (builtins.tryEval attrs).success then
-      if isDerivation attrs
-      then
-        if (builtins.tryEval attrs.drvPath).success
-        then { inherit (attrs) name drvPath; }
-        else { failed = true; }
-      else { recurseForDerivations = true; } //
-           mapAttrs (n: v: let path' = path ++ [n]; in trace path' (recurse path' v)) attrs
-    else { };
-
   mkHost = name: system: config: import <nixpkgs/nixos> { 
     inherit system;
     configuration = {
