647ffd8900af4db8bad3734708072edae6ca2dbe
David Guibert <david.guibert@gmail.com>
2019-02-13 09:12:04 +0100

    envrc: support for persistent cached shell
    see .envrc for details.

---
+++ admin/nixops/shell.nix
@@ -0,0 +1,28 @@
+{ versions ? import ./versions.nix
+, nixpkgs ? versions.nixpkgs
+, nur_dguibert ? builtins.getEnv "HOME" + "/nur-packages" # versions.nur_dguibert
+, home-manager ? builtins.getEnv "HOME" + "/code/home-manager" # versions.home-manager
+, overlays_ ? []
+
+, pkgs ? import nixpkgs {
+    config = (import "${nur_dguibert}/config.nix");
+    overlays = [
+      (import "${nur_dguibert}/overlays").default
+    ] ++ overlays_;
+  }
+}:
+let
+
+in with pkgs; lib.mkEnv {
+  name = "nixops";
+  buildInputs = [
+  ];
+  shellHook = ''
+    unset NIX_INDENT_MAKE
+    unset IN_NIX_SHELL
+    unset TMP
+    unset TMPDIR
+
+    export NIX_PATH=nixpkgs=${nixpkgs}:nur_dguibert=${nur_dguibert}:home-manager=${home-manager}:.:$NIX_PATH
+  '';
+}
