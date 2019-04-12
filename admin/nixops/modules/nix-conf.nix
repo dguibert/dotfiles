214bbf0aad9ed808887be526a9ab724c2861d793
David Guibert <david.guibert@gmail.com>
2019-04-12 15:43:02 +0200

    nix: sandobx=relaxed to allow __noChroot

--- admin/nixops/modules/nix-conf.nix
+++ admin/nixops/modules/nix-conf.nix
@@ -11,7 +11,7 @@ rec {
   zramSwap.enable = true;
   zramSwap.algorithm = "lzo";
 
-  nix.useSandbox = true;
+  nix.useSandbox = "relaxed";
   nix.autoOptimiseStore = true;
   nix.extraOptions = ''
      plugin-files = ${pkgs.nix-plugins.override { nix = config.nix.package; }}/lib/nix/plugins/libnix-extra-builtins.so
