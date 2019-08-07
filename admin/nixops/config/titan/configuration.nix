79504ca16c3fe4b07d17f1515fcb39b35147fbcc
David Guibert <david.guibert@gmail.com>
2019-08-07 15:42:39 +0200

    titan: set hydra

--- admin/nixops/config/titan/configuration.nix
+++ admin/nixops/config/titan/configuration.nix
@@ -213,4 +213,59 @@ rec {
   networking.nat.enable = true;
   networking.nat.internalInterfaces = ["ve-+"];
   networking.nat.externalInterface = "bond0";
+
+  services.hydra = {
+    enable = true;
+    hydraURL = "http://localhost:3000";
+    notificationSender = "hydra@orsin.freeboxos.fr";
+    port = 3000;
+    extraConfig = ''
+      store_uri = file:///var/lib/hydra/cache?secret-key=/etc/nix/hydra.orsin.freeboxos.fr-1/secret
+      #binary_cache_secret_key_file = /etc/nix/hydra.orsin.freeboxos.fr-1/secret
+    '';
+    buildMachinesFiles = [ /*"/etc/nix/machines"*/ ];
+  };
+  nix.buildMachines = [
+    {
+      hostName = "localhost";
+      systems = [ "x86_64-linux" "i686-linux" "aarch64-linux" ];
+      maxJobs = 16;
+      # for building VirtualBox VMs as build artifacts, you might need other
+      # features depending on what you are doing
+      supportedFeatures = [ "big-parallel" "kvm" ];
+    }
+  ];
+
+  services.postgresql = {
+    package = pkgs.postgresql94;
+    dataDir = "/var/db/postgresql-${config.services.postgresql.package.psqlSchema}";
+  };
+
+  systemd.services.hydra-manual-setup = {
+    description = "Create Admin User for Hydra";
+    serviceConfig.Type = "oneshot";
+    serviceConfig.RemainAfterExit = true;
+    wantedBy = [ "multi-user.target" ];
+    requires = [ "hydra-init.service" ];
+    after = [ "hydra-init.service" ];
+    environment = config.systemd.services.hydra-init.environment;
+    script = ''
+      if [ ! -e ~hydra/.setup-is-complete ]; then
+        # create admin user
+        /run/current-system/sw/bin/hydra-create-user dguibert --full-name 'David G. User' --email-address 'dguibert@orsin.freeboxos.fr' --password foobar --role admin
+        # create signing keys
+        /run/current-system/sw/bin/install -d -m 551 /etc/nix/hydra.orsin.freeboxos.fr-1
+        /run/current-system/sw/bin/nix-store --generate-binary-cache-key hydra.orsin.freeboxos.fr-1 /etc/nix/hydra.orsin.freeboxos.fr-1/secret /etc/nix/hydra.orsin.freeboxos.fr-1/public
+        /run/current-system/sw/bin/chown -R hydra:hydra /etc/nix/hydra.orsin.freeboxos.fr-1
+        /run/current-system/sw/bin/chmod 440 /etc/nix/hydra.orsin.freeboxos.fr-1/secret
+        /run/current-system/sw/bin/chmod 444 /etc/nix/hydra.orsin.freeboxos.fr-1/public
+        # create cache (https://qfpl.io/posts/nix/starting-simple-hydra/)
+        /run/current-system/sw/bin/install -d -m 755 /var/lib/hydra/cache
+        /run/current-system/sw/bin/chown -R hydra-queue-runner:hydra /var/lib/hydra/cache
+        # done
+        touch ~hydra/.setup-is-complete
+      fi
+    '';
+  };
+
 }
