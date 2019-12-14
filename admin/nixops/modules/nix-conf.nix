1d176ee8c8bd1903f3fbe6059aaecb48626f4b57
David Guibert <david.guibert@gmail.com>
2019-12-14 22:42:56 +0100

    nix: experimental Nix feature 'ca-references'

--- admin/nixops/modules/nix-conf.nix
+++ admin/nixops/modules/nix-conf.nix
@@ -14,7 +14,7 @@ rec {
   nix.useSandbox = "relaxed";
   nix.autoOptimiseStore = true;
   nix.extraOptions = ''
-    experimental-features = nix-command flakes
+    experimental-features = nix-command flakes ca-references
   '';
 #     plugin-files = ${pkgs.nix-plugins.override { nix = config.nix.package; }}/lib/nix/plugins/libnix-extra-builtins.so
 #   '';
