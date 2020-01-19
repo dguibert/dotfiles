8ba73f25bda348f5a64b7238bbf184b4b36c1566
David Guibert <david.guibert@gmail.com>
2020-01-19 19:16:11 +0100

    laptop-..: fix spice-client-glib-usb-acl-helper

--- admin/nixops/config/laptop-s93efa6b/configuration.nix
+++ admin/nixops/config/laptop-s93efa6b/configuration.nix
@@ -144,7 +144,7 @@ rec {
   systemd.services.libvirtd.environment.EBTABLES_PATH="${pkgs.ebtables}/bin/ebtables-legacy";
   # https://github.com/NixOS/nixpkgs/pull/35214#pullrequestreview-97783209
   security.wrappers.spice-client-glib-usb-acl-helper.source =
-    "${pkgs.spice_gtk}/bin/spice-client-glib-usb-acl-helper.real";
+    "${pkgs.spice_gtk}/bin/spice-client-glib-usb-acl-helper";
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
