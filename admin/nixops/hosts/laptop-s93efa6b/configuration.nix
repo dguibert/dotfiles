ba2996b46ecbf69169d3f0a127fa403c71287b57
David Guibert <david.guibert@gmail.com>
2020-02-24 21:25:53 +0100

    mq-deadline <-> kyber

--- admin/nixops/hosts/laptop-s93efa6b/configuration.nix
+++ admin/nixops/hosts/laptop-s93efa6b/configuration.nix
@@ -158,9 +158,9 @@ rec {
     # set scheduler for NVMe
     ACTION=="add|change", KERNEL=="nvme[0-9]*", ATTR{queue/scheduler}="none"
     # set scheduler for SSD and eMMC
-    ACTION=="add|change", KERNEL=="sd[a-z]|mmcblk[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="mq-deadline"
+    ACTION=="add|change", KERNEL=="sd[a-z]|mmcblk[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="kyber"
     # set scheduler for rotating disks
-    ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="kyber"
+    ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="mq-deadline"
 
     # tpm 2 devices need to be world readable
     SUBSYSTEM=="tpm", ACTION=="add", MODE="0666"
