c7dad69fc7db35cc8f16c5532b1cd0a05c2b1697
David Guibert <david.guibert@gmail.com>
2021-01-11 22:44:39 +0100

    make-iso -> .#nixosConfigurations.iso.config.system.build.isoImage

--- admin/nixops/flake.nix
+++ admin/nixops/flake.nix
@@ -337,6 +337,19 @@
     #  ];
     #};
 
+
+    ## nix build .#nixosConfigurations.iso.config.system.build.isoImage
+    nixosConfigurations.iso = nixpkgs.lib.nixosSystem {
+      modules = [
+        (import "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
+        ./modules/zfs.nix
+	./hosts/iso.nix
+        ({ config, lib, pkgs, resources, ... }: {
+          nixpkgs.localSystem.system = "x86_64-linux";
+	})
+      ];
+    };
+
     nixosConfigurations.titan = nixpkgs.lib.nixosSystem {
       modules = [
         ({ config, lib, pkgs, resources, ... }: {
