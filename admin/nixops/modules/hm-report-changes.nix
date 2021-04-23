7376ccd1d7866595ef127afae377aba892bdc86c
David Guibert <david.guibert@gmail.com>
2021-04-23 10:51:42 +0200

    module: nvd pretty echo

--- admin/nixops/modules/hm-report-changes.nix
+++ admin/nixops/modules/hm-report-changes.nix
@@ -4,6 +4,7 @@ with lib;
   options.home.report-changes.enable = mkEnableOption "report-changes";
   config = mkIf config.home.report-changes.enable {
     home.activation.report-changes = config.lib.dag.entryAnywhere ''
+      echo "Diffing: $oldGenPath $newGenPath"
       ${pkgs.nvd}/bin/nvd $oldGenPath $newGenPath
     '';
   };
