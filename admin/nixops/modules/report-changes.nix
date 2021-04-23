8ba0635b7fd6a8fedb745375db34fe44082c8d3e
David Guibert <david.guibert@gmail.com>
2021-04-23 11:02:10 +0200

    nvd: fix nixos systems diff

--- admin/nixops/modules/report-changes.nix
+++ admin/nixops/modules/report-changes.nix
@@ -4,9 +4,15 @@ with lib;
   options.report-changes.enable = mkEnableOption "report-changes";
   config = mkIf config.report-changes.enable {
     system.activationScripts.nvd = ''
-      echo "Diffing: $oldGenPath $newGenPath"
-      ${pkgs.nvd}/bin/nvd $oldGenPath $newGenPath
+      echo "Diffing: $(readlink /run/current-system) $systemConfig"
+      (
+      export PATH=${pkgs.nix}/bin:$PATH
+      ${pkgs.nvd}/bin/nvd /run/current-system $systemConfig
+      )
     '';
   };
 }
+
+
+
 
