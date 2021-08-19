cc6a61cf588a3bc13f09ce8e46fe31e5600a4153
David Guibert <david.guibert@gmail.com>
2021-08-19 22:13:02 +0200

    t580: kernel 5.12 -> 5.13

--- admin/nixops/hosts/t580/hardware-configuration.nix
+++ admin/nixops/hosts/t580/hardware-configuration.nix
@@ -28,7 +28,7 @@
     zfs rollback -r rt580/local/root@blank
   '';
 
-  boot.kernelPackages = pkgs.linuxPackages_5_12;
+  boot.kernelPackages = pkgs.linuxPackages_5_13;
   # https://lists.ubuntu.com/archives/kernel-team/2020-November/114986.html
   #boot.kernelPackages = pkgs.linuxPackages_testing;
   # *** ZFS Version: zfs-2.0.4-1
