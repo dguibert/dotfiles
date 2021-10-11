f3cf2ad36450e31fc5eeafed76fc6bed9b3e55fb
David Guibert <david.guibert@gmail.com>
2021-10-11 16:02:02 +0200

    tiny-ca: haproxy WIP

--- admin/nixops/roles/tiny-ca.nix
+++ admin/nixops/roles/tiny-ca.nix
@@ -15,6 +15,98 @@ in
   # openFirewall
 
   config = lib.mkIf cfg.enable {
+    ## https://github.com/NixOS/nixpkgs/pull/112322#issuecomment-780810817
+    #security.acme.server = "https://localhost:9443/acme/acme/directory";
+    #security.acme.email = "david.guibert+acme@gmail.com";
+    #security.acme.acceptTerms = true;
+    ## security.pki.certificateFiles = [ ../secrets/root-ca.crt ../secrets/intermediate_ca.crt ];
+    security.pki.certificateFiles = [ ../online-ca-orsin/certs/root_ca.crt
+                                      ../online-ca-orsin/certs/intermediate_ca.crt ];
+
+    networking.hosts = {
+      "192.168.1.24" = [ "jellyfin.local" ];
+    };
+    #services.nginx = {
+    #  enable = true;
+    #  virtualHosts."blog.example.com" = {
+    #    enableACME = true;
+    #    forceSSL = true;
+    #    root = "/var/www/blog";
+    #  };
+    #};
+    security.acme.acceptTerms = true;
+    security.acme.email = "david.guibert+certs@gmail.com";
+    security.acme.server = "https://localhost:9443/acme/acme/directory";
+    security.acme.validMinDays=1;
+    security.acme.certs."jellyfin.local" = {
+      group = config.users.groups.haproxy.name;
+      #extraLegoFlags=[ "--http.port" ":8888" "--tls" "--tls.port" ":8843" ];
+      extraLegoFlags=[ "--http.port" ":8888" "--dns.disable-cp" ];
+    };
+    # extraDomainNames
+    # group
+    # .extraLegoFlags --http-port
+    #security.acme.certs = {
+    #  "blog.example.com".email = "youremail@address.com";
+    #};
+    #https://serversforhackers.com/c/letsencrypt-with-haproxy
+    # sudo certbot certonly --standalone -d demo.scalinglaravel.com \
+    #    --non-interactive --agree-tos --email admin@example.com \
+    #    --http-01-port=8888
+
+    networking.firewall.interfaces.bond0.allowedTCPPorts = [
+      443 80
+    ];
+
+    services.haproxy.enable = true;
+    services.haproxy.config = ''
+      global
+              log /dev/log local0 debug
+
+      defaults
+          timeout connect 5s
+          timeout client 1m
+          timeout server 1m
+
+      frontend frontend-http
+          bind jellyfin.local:80
+          #bind :::80
+          mode http
+          # ACL for detecting Let's Encrypt validtion requests
+          acl is_certbot path_beg /.well-known/acme-challenge/
+          redirect scheme https code 301 if !is_certbot
+          use_backend letsencrypt_http if is_certbot
+
+      frontend jellyfin_https
+          mode http
+          #bind jellyfin.local:443 transparent ssl crt /var/lib/acme/jellyfin.local/full.pem ca-file ${../online-ca-orsin/certs/root_ca.crt} verify required alpn h2,http/1.1
+          bind jellyfin.local:443 ssl crt /var/lib/acme/jellyfin.local/full.pem ca-file ${../online-ca-orsin/certs/root_ca.crt} verify optional
+          # ACL for detecting Let's Encrypt validtion requests
+          acl is_certbot path_beg /.well-known/acme-challenge/
+          use_backend letsencrypt_http if is_certbot
+
+          default_backend jellyfin
+
+      backend jellyfin
+          mode http
+          http-request set-header X-Forwarded-Port %[dst_port]
+          http-request add-header X-Forwarded-Proto https if { ssl_fc }
+          server jellyfin 127.0.0.1:8096
+
+      backend letsencrypt_http
+          mode http
+          dispatch 127.0.0.1:8888
+          #server letsencrypt 127.0.0.1:8888
+
+    '';
+    ## Renew the certificate
+    #certbot renew --force-renewal --tls-sni-01-port=8888
+    ## Concatenate new cert files, with less output (avoiding the use tee and its output to stdout)
+    #bash -c "cat /etc/letsencrypt/live/demo.scalinglaravel.com/fullchain.pem /etc/letsencrypt/live/demo.scalinglaravel.com/privkey.pem > /etc/ssl/demo.scalinglaravel.com/demo.scalinglaravel.com.pem"
+    ## Reload  HAProxy
+    #service haproxy reload
+
+
     services.udev.extraRules = with pkgs; ''
       ATTR{idProduct}=="0407", ATTR{idVendor}=="1050", TAG+="systemd", SYMLINK="yubikey"
     '';
