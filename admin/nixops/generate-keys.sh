38840bf49323916d686922b2e01884cf1a38fe53
David Guibert <david.guibert@gmail.com>
2022-01-02 15:49:52 +0100

    generate-keys.sh: allow cli options

--- admin/nixops/generate-keys.sh
+++ admin/nixops/generate-keys.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 set -x
 host=$1; shift
-regenerate=${2:-false}
-host_sops_file=${3:-hosts/$host/secrets/secrets.yaml}
+regenerate=false
+host_sops_file=hosts/$host/secrets/secrets.yaml
 
 command -v sops
 
@@ -14,16 +14,52 @@ keyfile=$d/key
 options=""
 
 declare -a keys
-keys+=(wireguard_key)
-keys+=(ssh_host_rsa_key)
-keys+=(ssh_host_rsa_key.pub) # order matter (private key before pub one)
-keys+=(ssh_host_rsa_key-cert.pub)
-keys+=(ssh_host_ed25519_key)
-keys+=(ssh_host_ed25519_key.pub) # order matter (private key before pub one)
-keys+=(ssh_host_ed25519_key-cert.pub)
-keys+=(missing_key)
+#keys+=(wireguard_key)
+#keys+=(ssh_host_rsa_key)
+#keys+=(ssh_host_rsa_key.pub) # order matter (private key before pub one)
+#keys+=(ssh_host_rsa_key-cert.pub)
+#keys+=(ssh_host_ed25519_key)
+#keys+=(ssh_host_ed25519_key.pub) # order matter (private key before pub one)
+#keys+=(ssh_host_ed25519_key-cert.pub)
+#keys+=(missing_key)
+declare -A realms
+realms[titan]="titan,192.168.1.24,10.147.27.24"
 
-keys=($(nix eval .\#nixosConfigurations.$host.config.sops.secrets --json | jq -r '.[].key') )
+# Call getopt to validate the provided input.
+options=$(getopt -o rkf: --long key:,file: -- "$@")
+[ $? -eq 0 ] || {
+    echo "Incorrect options provided"
+    exit 1
+}
+eval set -- "$options"
+while true; do
+    case "$1" in
+    -r)
+        regenerate=true
+        ;;
+    -f|--file)
+        shift; # The arg is next in position args
+        host_sops_file=$1
+        ;;
+    -k|--key)
+        shift; # The arg is next in position args
+        if ! ${key_supplied:-true}; then
+            keys=()
+            key_supplied=true
+        fi
+        keys+=($1)
+        ;;
+    --)
+        shift
+        break
+        ;;
+    esac
+    shift
+done
+
+if ! ${key_supplied:-true}; then
+    keys=($(nix eval .\#nixosConfigurations.$host.config.sops.secrets --json | jq -r '.[].key') )
+fi
 
 for key in ${@:-${keys[@]}}; do
     echo "key ${key}"
@@ -44,9 +80,13 @@ for key in ${@:-${keys[@]}}; do
         esac
         # check if the key exists
         if ! sops  --extract '["'$key'"]' -d $sops_file > $keyfile; then
-            regenerate=true
+            regenerate_=true
         fi
-        if $regenerate; then
+        case "$key" in
+            *)
+                regenerate_=$regenerate
+        esac
+        if $regenerate_; then
             case "$key" in
                 wireguard_key)
                     wg genkey > $keyfile
@@ -69,14 +109,27 @@ for key in ${@:-${keys[@]}}; do
                 ssh_host_ed25519_key-cert.pub)
                     pass show ssh-ca/home > $d/ssh-ca
                     chmod 600 $d/ssh-ca
+                    ca=ssh-ca/home
+                    realms_=${realms[$host]}
 
+                    case "$key" in
+                        ssh_host_rsa_key-cert.pub)
+                            ssh-keygen -y -f <(sops --extract '["ssh_host_rsa_key"]' -d $sops_file) > $d/priv_key;;
+                        ssh_host_ed25519_key-cert.pub)
+                            ssh-keygen -y -f <(sops --extract '["ssh_host_ed25519_key"]' -d $sops_file) > $d/priv_key;;
+                        *)
+                            echo "ERROR: unknown key '$key'"
+                            exit 11
+                    esac
                     ssh-keygen -s $d/ssh-ca \
                         -P "$(pass show ${ca}-pass)" \
                         -I "$host host key" \
-                        -n "$realms" \
+                        -n "$realms_" \
                         -V -5m:+$(( 365 * 1))d \
                         -h \
-                        $keyfile
+                        $d/priv_key
+                    mv $d/priv_key-cert.pub $keyfile
+                    ssh-keygen -L -f $keyfile
                     ;;
                 id_buildfarm)
                     rm -f $keyfile
