69d566e2db9bb1cffee4472aedfaffabf340795f
David Guibert <david.guibert@gmail.com>
2022-01-03 16:57:44 +0100

    role libvirtd: enable swtpm

--- admin/nixops/roles/libvirtd.nix
+++ admin/nixops/roles/libvirtd.nix
@@ -13,7 +13,10 @@ in {
     # https://nixos.wiki/wiki/Virt-manager
     # https://nixos.org/nixops/manual/#idm140737318329504
     virtualisation.libvirtd.enable = true;
-    virtualisation.libvirtd.qemu.ovmf.package = pkgs.OVMF.override { secureBoot=true; tpmSupport=true; };
+    virtualisation.libvirtd.qemu = {
+      ovmf.package = pkgs.OVMF.override { secureBoot=true; tpmSupport=true; };
+      swtpm.enable = true;
+    };
     # https://github.com/NixOS/nixpkgs/issues/75878
     systemd.services.libvirtd.environment.EBTABLES_PATH="${pkgs.ebtables}/bin/ebtables-legacy";
     # https://github.com/NixOS/nixpkgs/pull/35214#pullrequestreview-97783209
@@ -25,7 +28,10 @@ in {
     };
 
     programs.dconf.enable = true;
-    environment.systemPackages = with pkgs; [ virt-manager ];
+    environment.systemPackages = with pkgs; [ virt-manager
+      ] ++ lib.optionals config.virtualisation.libvirtd.qemu.swtpm.enable [
+        config.virtualisation.libvirtd.qemu.swtpm.package
+      ];
 
     systemd.tmpfiles.rules = [ "d /var/lib/libvirt/images 1770 root libvirtd -" ];
   };
