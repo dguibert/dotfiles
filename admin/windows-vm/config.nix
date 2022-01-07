5264f2e1d7db73a271ac36b20aefc97a2a677121
David Guibert <david.guibert@gmail.com>
2022-01-07 21:23:37 +0100

    windows-vm: set machine q35 (need to set cdrom from ide to sata)

--- admin/windows-vm/config.nix
+++ admin/windows-vm/config.nix
@@ -1,6 +1,16 @@
 { config, lib, pkgs, ... }:
   let
 
+    disk_fun = { volume_id  ?  null
+               , block_device ? null
+               , file ? null
+               , scsi ? null
+               , url ? null
+               , wwn ? null
+       }: {
+         inherit volume_id block_device file scsi url wwn;
+       };
+
 in {
   terraform.required_providers.libvirt.source = "dmacvicar/libvirt";
   # Configure the Libvirt provider
@@ -20,16 +30,19 @@ in {
     # This file is usually present as part of the ovmf firmware package in many
     # Linux distributions.
     #firmware = "/usr/share/OVMF/OVMF_CODE.fd"
-    firmware = "/run/libvirt/nix-ovmf/OVMF_CODE.fd";
+    firmware = "/run/libvirt/nix-ovmf/OVMF_CODE.secboot.fd";
     nvram = {
       file = "/var/lib/libvirt/qemu/nvram/${name}_VARS.fd";
-      template = "/run/libvirt/nix-ovmf/OVMF_VARS.fd";
+      template = "/run/libvirt/nix-ovmf/OVMF_VARS.secboot.fd";
       #template = "/tmp/keys/OVMFKeys/OVMF_VARS.fd";
     };
     network_interface = {
       network_name = "default";
       wait_for_lease = true;
     };
+    cpu.mode = "host-passthrough";
+    machine="pc-q35-6.1";
+    #machine="q35";
 
     # IMPORTANT
     # Ubuntu can hang if an isa-serial is not present at boot time.
@@ -58,31 +71,17 @@ in {
 
     disk = [
       #{ volume_id = "${"$"}{libvirt_volume.win1-qcow2.id}"; } # known only after apply
-      { volume_id = null;
-        block_device=null;
-        file=null;
-        scsi=null;
+      (disk_fun {
+#     #   file="/tmp/SW_DVD9_Win_Pro_10_20H2.12_64BIT_French_Pro_Ent_EDU_N_MLF_X22-87968.ISO";
+        file="/tmp/Win10_21H2_French_x64.iso";
+      })
+      (disk_fun {
         #url="https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/latest-virtio/virtio-win.iso";
         url="https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso";
-        wwn=null;
-      }
-      #{ volume_id = null;
-      #  block_device=null;
-#     #   file="/tmp/SW_DVD9_Win_Pro_10_20H2.12_64BIT_French_Pro_Ent_EDU_N_MLF_X22-87968.ISO";
-      #  file="/tmp/Win10_21H2_French_x64.iso";
-      #  scsi=null;
-      #  url=null;
-      #  wwn=null;
-      #}
-      { #volume_id = config.resource.libvirt_volume.win1-qcow2.id;
-	#volume_id = "libvirt_volume.win1-qcow2.id";
+      })
+      (disk_fun {
         volume_id = "\${ libvirt_volume.win1-qcow2.id }";
-        block_device=null;
-        file=null;
-        scsi=false;
-        url=null;
-        wwn=null;
-      }
+      })
     ];
     tpm = {
       backend_type    = "emulator";
