85294ca2d90b7a61714b23a024aeb5c3fe14cb8b
David Guibert <david.guibert@gmail.com>
2022-11-20 22:20:19 +0100

    hm: no user dbus

--- admin/nixops/users/dguibert/module-dwl.nix
+++ admin/nixops/users/dguibert/module-dwl.nix
@@ -122,33 +122,33 @@ with lib; {
     };
   };
 
-  systemd.user.sockets.dbus = {
-    Unit = {
-      Description = "D-Bus User Message Bus Socket";
-    };
-    Socket = {
-      ListenStream = "%t/bus";
-      ExecStartPost = "${pkgs.systemd}/bin/systemctl --user set-environment DBUS_SESSION_BUS_ADDRESS=unix:path=%t/bus";
-    };
-    Install = {
-      WantedBy = [ "sockets.target" ];
-      Also = [ "dbus.service" ];
-    };
-  };
+  #systemd.user.sockets.dbus = {
+  #  Unit = {
+  #    Description = "D-Bus User Message Bus Socket";
+  #  };
+  #  Socket = {
+  #    ListenStream = "%t/bus";
+  #    ExecStartPost = "${pkgs.systemd}/bin/systemctl --user set-environment DBUS_SESSION_BUS_ADDRESS=unix:path=%t/bus";
+  #  };
+  #  Install = {
+  #    WantedBy = [ "sockets.target" ];
+  #    Also = [ "dbus.service" ];
+  #  };
+  #};
 
-  systemd.user.services.dbus = {
-    Unit = {
-      Description = "D-Bus User Message Bus";
-      Requires = [ "dbus.socket" ];
-    };
-    Service = {
-      ExecStart = "${pkgs.dbus}/bin/dbus-daemon --session --address=systemd: --nofork --nopidfile --systemd-activation";
-      ExecReload = "${pkgs.dbus}/bin/dbus-send --print-reply --session --type=method_call --dest=org.freedesktop.DBus / org.freedesktop.DBus.ReloadConfig";
-    };
-    Install = {
-      Also = [ "dbus.socket" ];
-    };
-  };
+  #systemd.user.services.dbus = {
+  #  Unit = {
+  #    Description = "D-Bus User Message Bus";
+  #    Requires = [ "dbus.socket" ];
+  #  };
+  #  Service = {
+  #    ExecStart = "${pkgs.dbus}/bin/dbus-daemon --session --address=systemd: --nofork --nopidfile --systemd-activation";
+  #    ExecReload = "${pkgs.dbus}/bin/dbus-send --print-reply --session --type=method_call --dest=org.freedesktop.DBus / org.freedesktop.DBus.ReloadConfig";
+  #  };
+  #  Install = {
+  #    Also = [ "dbus.socket" ];
+  #  };
+  #};
 
   #systemd.user.services.dwl = {
   #  Unit = {
