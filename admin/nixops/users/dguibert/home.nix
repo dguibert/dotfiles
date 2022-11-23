2ba0f4c3b602e46081682a04ecdf23249046dbec
David Guibert <david.guibert@gmail.com>
2022-11-23 14:31:16 +0100

    update flake.lock
    • Updated input 'nixpkgs':
        'git+file:///home/dguibert/code/nixpkgs?ref=refs%2fheads%2fpu&rev=bec562887bd464325d5a44c9ddfabcc24f806ac3' (2022-11-21)
      → 'github:dguibert/nixpkgs/9807940882bf65302e08de4a5f46288bb33c19f8' (2022-11-23)

--- admin/nixops/users/dguibert/home.nix
+++ admin/nixops/users/dguibert/home.nix
@@ -829,7 +829,6 @@ let
           programs.firefox = {
             enable = true;
             package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
-              forceWayland = true;
               extraPolicies = {
                 ExtensionSettings = { };
               };
