cdeea255a5dab6f8ab8685cd25d9bb107e7d27a4
David Guibert <david.guibert@gmail.com>
2023-01-05 09:53:14 +0100

    spartan: custom profile support

--- admin/nixops/homes/bguibertd@spartan-x86_64.nix
+++ admin/nixops/homes/bguibertd@spartan-x86_64.nix
@@ -3,9 +3,12 @@
   imports = [
     ./dguibert/home.nix
     ./dguibert/emacs.nix
+    ./dguibert/custom-profile.nix
   ];
   centralMailHost.enable = false;
   withGui.enable = false;
+  withCustomProfile.enable = true;
+  withCustomProfile.suffix = "x86_64";
 
   nixpkgs.overlays = [
     inputs.nur_dguibert.overlays.cluster
@@ -15,31 +18,16 @@
   home.homeDirectory = "/home_nfs/bguibertd";
   home.stateVersion = "22.11";
 
-  home.sessionVariables.NIX_STATE_DIR = "${pkgs.nixStore}/var/nix";
-  home.sessionVariables.NIX_PROFILE = "${pkgs.nixStore}/var/nix/profiles/per-user/${config.home.username}/profile-x86_64";
-  home.activation.setNixVariables = lib.hm.dag.entryBefore [ "writeBoundary" "checkLinkTargets" "checkFilesChanges" ]
-    ''
-      set -x
-      export NIX_STATE_DIR=${config.home.sessionVariables.NIX_STATE_DIR}
-      export NIX_PROFILE=${config.home.sessionVariables.NIX_PROFILE}
-      export PATH=${pkgs.nix}/bin:$PATH
-      rm -rf ${config.home.profileDirectory}
-      ln -sf ${config.home.sessionVariables.NIX_PROFILE} ${config.home.profileDirectory}
-      export HOME_MANAGER_BACKUP_EXT=bak
-      nix-env --set-flag priority 80 nix || true
-      set +x
-    '';
-  # [[ -f ~/.profile.$(uname -m) ]] && . ~/.profile.$(uname -m)
-  programs.bash.bashProfileFile = ".bash_profile.x86_64";
-  programs.bash.bashrcFile = ".bashrc.x86_64";
-  programs.bash.profileFile = ".profile.x86_64";
-  programs.bash.bashLogoutFile = ".bash_logout.x86_64";
+  home.sessionPath = [
+    "${pkgs.nix}/bin"
+  ];
 
-  home.profileDirectory = lib.mkForce "${config.home.homeDirectory}/.nix-profile-x86_64";
+  home.packages = with pkgs; [
+    xpra
+    bashInteractive
 
-  home.sessionVariablesFileName = "hm-x86_64-session-vars.sh";
-  home.sessionVariablesGuardVar = "__HM_X86_64_SESS_VARS_SOURCED";
-  home.pathName = "home-manager-x86_64_path";
-  home.gcLinkName = "current-home-x86_64";
-  home.generationLinkNamePrefix = "home-manager-x86_64";
+    datalad
+    git-annex
+  ];
+
 }
--- admin/nixops/homes/bguibertd@spartan.nix
+++ admin/nixops/homes/bguibertd@spartan.nix
@@ -3,9 +3,12 @@
   imports = [
     ./dguibert/home.nix
     ./dguibert/emacs.nix
+    ./dguibert/custom-profile.nix
   ];
   centralMailHost.enable = false;
   withGui.enable = false;
+  withCustomProfile.enable = true;
+  withCustomProfile.suffix = "";
 
   nixpkgs.overlays = [
     inputs.nur_dguibert.overlays.cluster
@@ -15,12 +18,7 @@
   home.homeDirectory = "/home_nfs/bguibertd";
   home.stateVersion = "22.11";
   #home.activation.setNixVariables = lib.hm.dag.entryBefore ["writeBoundary"]
-  home.sessionVariables.NIX_STATE_DIR = "${pkgs.nixStore}/var/nix";
-  home.sessionVariables.NIX_PROFILE = "${pkgs.nixStore}/var/nix/profiles/per-user/${config.home.username}/profile";
   programs.bash.bashrcExtra = /*(homes.withoutX11 args).programs.bash.initExtra +*/ ''
-    export NIX_STATE_DIR=${config.home.sessionVariables.NIX_STATE_DIR}
-    export NIX_PROFILE=${config.home.sessionVariables.NIX_PROFILE}
-    export PATH=$NIX_PROFILE/bin:$PATH:${pkgs.nix}/bin
     # support for x86_64/aarch64
     # include .bashrc if it exists
     [[ -f ~/.bashrc.$(uname -m) ]] && . ~/.bashrc.$(uname -m)
@@ -30,28 +28,5 @@
     # include .profile if it exists
     [[ -f ~/.profile.$(uname -m) ]] && . ~/.profile.$(uname -m)
   '';
-  home.activation.setNixVariables = lib.hm.dag.entryBefore [ "writeBoundary" "checkLinkTargets" "checkFilesChanges" ]
-    ''
-      set -x
-      export NIX_STATE_DIR=${config.home.sessionVariables.NIX_STATE_DIR}
-      export NIX_PROFILE=${config.home.sessionVariables.NIX_PROFILE}
-      export PATH=${pkgs.nix}/bin:$PATH
-      rm -rf ${config.home.profileDirectory}
-      ln -sf ${config.home.sessionVariables.NIX_PROFILE} ${config.home.profileDirectory}
-      export HOME_MANAGER_BACKUP_EXT=bak
-      nix-env --set-flag priority 80 nix || true
-      set +x
-    '';
-  home.sessionPath = [
-    "${pkgs.nix}/bin"
-  ];
-
-  home.packages = with pkgs; [
-    xpra
-    bashInteractive
-
-    datalad
-    git-annex
-  ];
 
 }
