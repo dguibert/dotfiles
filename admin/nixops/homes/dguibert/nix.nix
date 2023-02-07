1dce31510a927293f60b8bca9bc7fc5602fc8dee
David Guibert <david.guibert@gmail.com>
2023-02-07 09:02:00 +0100

    hm[nix]: commit nix.nix file

---
+++ admin/nixops/homes/dguibert/nix.nix
@@ -0,0 +1,14 @@
+{ lib, config, pkgs, inputs, ... }:
+{
+  options.withNix.enable = (lib.mkEnableOption "Enable nix config") // { default = true; };
+
+  config = lib.mkIf config.withNix.enable {
+    nix.registry = lib.mapAttrs
+      (id: flake: {
+        inherit flake;
+        from = { inherit id; type = "indirect"; };
+      })
+      inputs;
+  };
+}
+
