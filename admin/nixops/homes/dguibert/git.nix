c672a2a238625b2bea98ecdb90dbb3e615a46f5d
David Guibert <david.guibert@gmail.com>
2023-02-26 15:46:22 +0100

    git: multiple credential helper

--- admin/nixops/homes/dguibert/git.nix
+++ admin/nixops/homes/dguibert/git.nix
@@ -19,7 +19,11 @@
   programs.git.iniContent.rerere.enabled = true;
   programs.git.iniContent.rerere.autoupdate = true;
   programs.git.iniContent.rebase.autosquash = true;
-  programs.git.iniContent.credential.helper = "password-store";
+  programs.git.iniContent.credential.helper = [
+    "password-store"
+    "store"
+    "cache --timeout 86400"
+  ];
   programs.git.iniContent."url \"software.ecmwf.int\"".insteadOf = "ssh://git@software.ecmwf.int:7999";
   programs.git.iniContent.color.branch = "auto";
   programs.git.iniContent.color.diff = "auto";
