0d4787decc6bdf3a0277f39f7cee923dc5ddf2ee
David Guibert <david.guibert@gmail.com>
2023-04-11 21:56:58 +0200

    pre-commit: fix src attribute

--- admin/nixops/checks/pre-commit.nix
+++ admin/nixops/checks/pre-commit.nix
@@ -3,7 +3,7 @@
   perSystem = { config, self', inputs', pkgs, system, ... }: {
     checks = {
       pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
-        src = ./.;
+        src = ./..;
         hooks = {
           nixpkgs-fmt.enable = true;
           prettier.enable = true;
