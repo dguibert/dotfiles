faeb272581d7da6465d0827190c761cb0e54c8e1
David Guibert <david.guibert@gmail.com>
2023-07-02 20:35:06 +0200

    role-zigbee: try setting acl (2)

--- admin/nixops/modules/nixos/role-zigbee.nix
+++ admin/nixops/modules/nixos/role-zigbee.nix
@@ -63,14 +63,14 @@ in
             "readwrite #"
           ];
           # nix shell nixpkgs#mosquitto --command mosquitto_passwd -c /tmp/password zigbee
-          password = "$7$101$hjkpxbnBRKvg9ZdL$wlF214j+mWx17ccKDapsnBzcfsZiDGkM9f/ugKOw7GAwYttG+mdtWVpkakB6mee0i7lJl102lnmu48BoVKpfmg==";
+          hashedPassword = "$7$101$hjkpxbnBRKvg9ZdL$wlF214j+mWx17ccKDapsnBzcfsZiDGkM9f/ugKOw7GAwYttG+mdtWVpkakB6mee0i7lJl102lnmu48BoVKpfmg==";
         };
         users.root = {
           acl = [
             "readwrite #"
           ];
           # nix shell nixpkgs#mosquitto --command mosquitto_passwd -c /tmp/password root
-          password = "$7$101$hjkpxbnBRKvg9ZdL$wlF214j+mWx17ccKDapsnBzcfsZiDGkM9f/ugKOw7GAwYttG+mdtWVpkakB6mee0i7lJl102lnmu48BoVKpfmg==";
+          hashedPassword = "$7$101$hjkpxbnBRKvg9ZdL$wlF214j+mWx17ccKDapsnBzcfsZiDGkM9f/ugKOw7GAwYttG+mdtWVpkakB6mee0i7lJl102lnmu48BoVKpfmg==";
         };
       }
     ];
