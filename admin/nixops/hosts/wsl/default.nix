31c73057a56d1f76cbd0e34708d1d359deab5f19
David Guibert <david.guibert@gmail.com>
2023-11-15 21:23:30 +0100

    wsl: add dguibert profile

---
+++ admin/nixops/hosts/wsl/default.nix
@@ -0,0 +1,58 @@
+{ config, lib, inputs, withSystem, self, ... }:
+{
+  options.modules.hosts.wsl = lib.mkOption {
+    type = lib.types.listOf lib.types.raw;
+    default = [ ];
+  };
+
+  config.modules.hosts.wsl = [
+    inputs.nixos-wsl.nixosModules.wsl
+    ({ ... }: {
+      wsl.enable = true;
+      wsl.defaultUser = "dguibert";
+      wsl.startMenuLaunchers = true;
+
+      programs.bash.loginShellInit = "nixos-wsl-welcome";
+    })
+    ../../modules/nixos/nix-conf.nix
+    inputs.home-manager.nixosModules.home-manager
+    ../../users/dguibert
+    ({ pkgs, ... }: {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.backupFileExtension = "hm-backup";
+      home-manager.extraSpecialArgs = {
+        inherit inputs pkgs;
+        sopsDecrypt_ = pkgs.sopsDecrypt_;
+      };
+
+      home-manager.users.dguibert = {
+        imports = [
+          ({ config, pkgs, ... }: {
+            imports = [
+              ../../modules/home-manager/dguibert.nix
+            ];
+            withGui.enable = false;
+            withEmacs.enable = true;
+            home.homeDirectory = "/home/dguibert";
+            home.stateVersion = "23.05";
+          })
+        ];
+      };
+    })
+
+  ];
+
+  config.flake.nixosConfigurations = withSystem "x86_64-linux" ({ system, ... }: {
+    wsl = inputs.nixpkgs.lib.nixosSystem {
+      inherit system;
+
+      specialArgs = {
+        pkgs = self.legacyPackages.${system};
+        inherit inputs;
+      };
+      modules = config.modules.hosts.wsl;
+    };
+  });
+}
+
