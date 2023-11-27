eb4f268813469326708d02e8bde15edacc10abc1
David Guibert <david.guibert@gmail.com>
2023-11-27 13:53:31 +0100

    gen-home-manager-configuration: allow cross compilation

--- admin/nixops/lib/gen-home-manager-configuration.nix
+++ admin/nixops/lib/gen-home-manager-configuration.nix
@@ -5,15 +5,24 @@ system: name: { config, lib, inputs, withSystem, self, ... }: {
     default = [ ];
   };
 
+  options.modules.homes."${name}-cross-system" = lib.mkOption {
+    type = lib.types.nullOr lib.types.str;
+    default = null;
+  };
+
   config.modules.homes."${name}" = [ ];
 
   config.flake.homeConfigurations =
-    withSystem system ({ system, pkgs, ... }: {
+    withSystem system ({ system, pkgs, ... }: let
+          pkgs' = if config.modules.homes."${name}-cross-system" != null then
+            pkgs.pkgsCross.${config.modules.homes."${name}-cross-system"} else pkgs;
+      in {
       "${name}" = inputs.home-manager.lib.homeManagerConfiguration
         {
-          inherit pkgs;
+          pkgs = pkgs';
           extraSpecialArgs = {
-            inherit inputs pkgs;
+            inherit inputs;
+            pkgs = pkgs';
             sopsDecrypt_ = pkgs.sopsDecrypt_;
           };
           modules = config.modules.homes."${name}";
