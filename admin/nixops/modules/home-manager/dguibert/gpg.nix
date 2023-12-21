c8da3c6e2b963112edcb49df00f688b2ae5371e7
David Guibert <david.guibert@gmail.com>
2023-12-21 17:16:15 +0100

    hm: pinentry flavour gtk2 -> gnome3

--- admin/nixops/modules/home-manager/dguibert/gpg.nix
+++ admin/nixops/modules/home-manager/dguibert/gpg.nix
@@ -2,8 +2,7 @@
 {
   services.gpg-agent.pinentryFlavor =
     if config.withGui.enable
-    #then "gnome3" # No Gcr System Prompter available : Gnome Key Ring prompter tool ?
-    then "gtk2"
+    then "gnome3" # No Gcr System Prompter available : Gnome Key Ring prompter tool ?
     else "curses";
 
   services.gpg-agent.enable = true;
