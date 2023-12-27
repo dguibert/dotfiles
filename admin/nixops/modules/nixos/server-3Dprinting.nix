02d6c27430a39ffe210d56257a248758ba2d1f8a
David Guibert <david.guibert@gmail.com>
2023-12-27 21:09:51 +0100

    fix: Imprecise conversion from float to string 22.251426

--- admin/nixops/modules/nixos/server-3Dprinting.nix
+++ admin/nixops/modules/nixos/server-3Dprinting.nix
@@ -167,7 +167,7 @@ in
           full_steps_per_rotation = 200; # 1.8 degree motor
           # See calibrating rotation_distance on extruders doc
           #rotation_distance = 21.54087;
-          rotation_distance = 22.251425904873;
+          rotation_distance = "22.251425904873";
           gear_ratio = "50:10"; # For Mini Afterburner
           microsteps = 32;
           nozzle_diameter = 0.400;
