bf0205b84cc088ffe2b3af753c013fe8da75c00e
David Guibert <david.guibert@gmail.com>
2024-03-19 20:48:01 +0100

    prometheus: try setting grafana dashboards

--- modules/flake-parts/services.nix
+++ modules/flake-parts/services.nix
@@ -259,12 +259,47 @@ let
   ];
 
   prometheus-server = [
-    ({ config, lib, pkgs, inputs, ... }: {
+    ({ config, lib, pkgs, inputs, ... }: let 
+      # Copy all json dashboard
+      grafana-dashboards = pkgs.stdenv.mkDerivation {
+        name = "grafana-dashboards";
+        src = ./dashboards;
+        installPhase = ''
+      mkdir -p $out/
+      install -D -m755 $src/*.json $out/
+    '';
+      };
+    in {
       services.grafana = {
         enable = true;
-        domain = "grafana.rpi41";
-        port = 2342;
-        addr = "127.0.0.1";
+        settings = {
+          analytics.reporting_enabled = false;
+
+          server = {
+            domain = "grafana.rpi41";
+            port = 2342;
+            addr = "127.0.0.1";
+          };
+        };
+        provision.datasources.settings.datasources = [
+          {
+            name = "Prometheus";
+            type = "prometheus";
+            access = "proxy";
+            url = "http://127.0.0.1:${toString config.services.prometheus.port}";
+          }
+          {
+            name = "Loki";
+            type = "loki";
+            access = "proxy";
+            url = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}";
+          }
+        ];
+
+        provision.dashboards.settings.providers = [{
+          name = "default";
+          options.path = grafana-dashboards;
+        }];
       };
       services.prometheus = {
         enable = true;
@@ -280,6 +315,80 @@ let
         ];
 
       };
+      services.loki = {
+        enable = true;
+        configuration = {
+          server.http_listen_port = 3100;
+          auth_enabled = false;
+
+          ingester = {
+            lifecycler = {
+              address = "127.0.0.1";
+              ring = {
+                kvstore = {
+                  store = "inmemory";
+                };
+                replication_factor = 1;
+              };
+            };
+            chunk_idle_period = "1h";
+            max_chunk_age = "1h";
+            chunk_target_size = 999999;
+            chunk_retain_period = "30s";
+            max_transfer_retries = 0;
+          };
+
+          schema_config = {
+            configs = [{
+              from = "2022-06-06";
+              store = "boltdb-shipper";
+              object_store = "filesystem";
+              schema = "v11";
+              index = {
+                prefix = "index_";
+                period = "24h";
+              };
+            }];
+          };
+
+          storage_config = {
+            boltdb_shipper = {
+              active_index_directory = "/var/lib/loki/boltdb-shipper-active";
+              cache_location = "/var/lib/loki/boltdb-shipper-cache";
+              cache_ttl = "24h";
+              shared_store = "filesystem";
+            };
+
+            filesystem = {
+              directory = "/var/lib/loki/chunks";
+            };
+          };
+
+          limits_config = {
+            reject_old_samples = true;
+            reject_old_samples_max_age = "168h";
+          };
+
+          chunk_store_config = {
+            max_look_back_period = "0s";
+          };
+
+          table_manager = {
+            retention_deletes_enabled = false;
+            retention_period = "0s";
+          };
+
+          compactor = {
+            working_directory = "/var/lib/loki";
+            shared_store = "filesystem";
+            compactor_ring = {
+              kvstore = {
+                store = "inmemory";
+              };
+            };
+          };
+        };
+      };
     })
   ];
 
@@ -294,7 +403,32 @@ let
           };
         };
       };
+      services.promtail = {
+        enable = true;
+        configuration = {
+          server = {
+            http_listen_port = 9080;
+            grpc_listen_port = 0;
+          };
+          clients = [ { url = "http://localhost:3100/loki/api/v1/push"; } ];
+          scrape_configs = [{
+            job_name = "journal";
+            journal = {
+              max_age = "12h";
+              labels = {
+                job = "systemd-journal";
+                host = config.networking.hostName;
+              };
+            };
+            relabel_configs = [{
+              source_labels = [ "__journal__systemd_unit" ];
+              target_label = "unit";
+            }];
+          }];
+        };
+      };
     })
+
   ];
 in
 {
