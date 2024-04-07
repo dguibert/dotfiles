c463d6b568b3f0d3f68d8e2f9dbfd3d303db0daa
David Guibert <david.guibert@gmail.com>
2024-04-07 22:49:46 +0200

    hm: zellij from hm module

--- modules/home-manager/dguibert/zellij.nix
+++ modules/home-manager/dguibert/zellij.nix
@@ -3,9 +3,38 @@
   options.withZellij.enable = (lib.mkEnableOption "Enable Zellij config"); # // { default = true; };
 
   config = lib.mkIf config.withZellij.enable {
-    home.packages = with pkgs; [
-      zellij
-    ];
-    # .config/zellij/config.kdl
+    programs.zellij.enable = true;
+
+    programs.zellij.settings = {
+      keybinds = {
+        unbind = "Ctrl q"; # unbind in all modes
+
+        locked = {
+          unbind = "Ctrl g";
+          bind = {
+            _args = [ "Alt g" ];
+            SwitchToMode = "normal";
+          };
+        };
+      };
+
+      # default_layout "compact"
+      default_mode = "locked";
+      copy_command = "wl-copy";
+      pane_frames = false;
+      # copy_clipboard "primary"
+
+      pane = {
+        _args = [
+          "size = 1"
+          "borderless = true"
+        ];
+        plugin = {
+          _props = { location = "zellij:compact-bar"; };
+        };
+      };
+    };
+
+    programs.zellij.enableBashIntegration = true;
   };
 }
