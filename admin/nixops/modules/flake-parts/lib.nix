2d2eeb7925d2cc07f204bf32236d55bb404fd29c
David Guibert <david.guibert@gmail.com>
2024-05-24 21:47:04 +0200

    admin/nixops: move to master branch

--- admin/nixops/modules/flake-parts/lib.nix
+++
@@ -1,9 +0,0 @@
-{ self, lib, inputs, ... }: {
-  flake.lib =
-    let
-      l = lib // builtins;
-    in
-    inputs.nixpkgs.lib // {
-      genHomeManagerConfiguration = import ../../lib/gen-home-manager-configuration.nix { inherit lib; };
-    };
-}
--- admin/nixops/users/default.nix
+++
@@ -1,9 +0,0 @@
-{ ... }: {
-  imports = [
-    (import ./root/default.nix)
-    (import ./dguibert/default.nix)
-  ];
-
-  users.mutableUsers = false;
-
-}
